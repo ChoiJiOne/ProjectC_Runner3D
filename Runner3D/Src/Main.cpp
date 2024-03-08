a4eca91f84096a728524ecab5ffa808bed6be7de
DESKTOP-CIQEUKA\choij <choijione02@gmail.com>
2024-03-09 07:56:59 +0900

    본격적인 개발 진행을 위한 매니저 초기화/해제 및 루프 추가

--- Runner3D/Src/Main.cpp
+++ Runner3D/Src/Main.cpp
@@ -1,7 +1,40 @@
 #include <cstdint>
+#include <functional>
 #include <windows.h>
+
+#include "CrashModule.h"
+
+#include "Assertion.h"
+#include "InputManager.h"
+#include "RenderManager.h"
+#include "ResourceManager.h"
+#include "SDLManager.h"
+
+bool bIsDone = false;
 
 int32_t WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR pCmdLine, _In_ int32_t nCmdShow)
 {
+	CHECK(CrashModule::RegisterExceptionFilter());
+
+	SDLManager::Get().Startup();
+	InputManager::Get().Startup();
+	ResourceManager::Get().Startup();
+	RenderManager::Get().Startup();
+
+	InputManager::Get().AddWindowEventAction(EWindowEvent::CLOSE, [&]() {bIsDone = true; }, true);
+
+	while (!bIsDone)
+	{
+		InputManager::Get().Tick();
+
+		RenderManager::Get().BeginFrame(0.0f, 0.0f, 0.0f, 1.0f);
+		RenderManager::Get().EndFrame();
+	}
+	
+	ResourceManager::Get().Shutdown();
+	RenderManager::Get().Shutdown();
+	InputManager::Get().Shutdown();
+	SDLManager::Get().Shutdown();
+
 	return 0;
 }
