9b58223e68b67c3b442fc7fe142720cb32cd7d64
CHOIJIONE\choij <choijione02@gmail.com>
2024-03-09 14:48:55 +0900

    쿼터니언과 벡터의 곱셈 연산을 수행하는 기능 추가

--- Module/MathModule/Inc/Quat.h
+++ Module/MathModule/Inc/Quat.h
@@ -10,7 +10,7 @@ struct Quat
 {
 	/**
 	 * @brief 쿼터니언의 기본 생성자입니다.
-	 * 
+	 *
 	 * @note 모든 원소의 값을 0으로 초기화합니다.
 	 */
 	Quat() noexcept : x(0.0f), y(0.0f), z(0.0f), w(1.0f) {}
@@ -18,7 +18,7 @@ struct Quat
 
 	/**
 	 * @brief 쿼터니언의 생성자입니다.
-	 * 
+	 *
 	 * @param xx 쿼터니언의 x 성분입니다.
 	 * @param yy 쿼터니언의 y 성분입니다.
 	 * @param zz 쿼터니언의 z 성분입니다.
@@ -49,7 +49,7 @@ struct Quat
 
 	/**
 	 * @brief 쿼터니언의 생성자입니다.
-	 * 
+	 *
 	 * @param vv i,j,k 축의 벡터 요소입니다.
 	 * @param ww 회전의 스케일을 조정하는 w 성분입니다.
 	 */
@@ -58,10 +58,10 @@ struct Quat
 
 	/**
 	 * @brief 쿼터니언의 복사 생성자입니다.
-	 * 
+	 *
 	 * @param q 원소를 복사할 쿼터니언 구조체의 인스턴스입니다.
 	 */
-	Quat(Quat&& q) noexcept  : x(q.x), y(q.y), z(q.z), w(q.w) {}
+	Quat(Quat&& q) noexcept : x(q.x), y(q.y), z(q.z), w(q.w) {}
 
 
 	/**
@@ -74,9 +74,9 @@ struct Quat
 
 	/**
 	 * @brief 쿼터니언의 대입 연산자입니다.
-	 * 
+	 *
 	 * @param q 원소를 복사할 쿼터니언 구조체의 인스턴스입니다.
-	 * 
+	 *
 	 * @return 대입한 쿼터니언의 참조자를 반환합니다.
 	 */
 	Quat& operator=(Quat&& q) noexcept
@@ -114,7 +114,7 @@ struct Quat
 
 	/**
 	 * @brief 쿼터니언에 -부호를 취합니다.
-	 * 
+	 *
 	 * @return 쿼터니언에 -부호를 취한 새로운 벡터를 반환합니다.
 	 */
 	Quat operator-() const
@@ -125,9 +125,9 @@ struct Quat
 
 	/**
 	 * @brief 두 쿼터니언에 대응하는 원소를 더합니다.
-	 * 
+	 *
 	 * @param q 쿼터니언의 덧셈을 수행할 피연산자입니다.
-	 * 
+	 *
 	 * @return 두 쿼터니언에 대응하는 원소를 더한 결과를 반환합니다.
 	 */
 	Quat operator+(Quat&& q) const
@@ -177,9 +177,9 @@ struct Quat
 
 	/**
 	 * @brief 쿼터니언의 스케일 연산을 수행합니다.
-	 * 
+	 *
 	 * @param s 스케일 연산을 수행할 값입니다.
-	 * 
+	 *
 	 * @return 스케일 연산을 수행한 결과를 반환합니다.
 	 */
 	Quat operator*(float s) const
@@ -208,9 +208,9 @@ struct Quat
 
 	/**
 	 * @brief 쿼터니언 곱셈 연산을 수행합니다.
-	 * 
+	 *
 	 * @param q 곱셈 연산을 수행할 쿼터니언 값입니다.
-	 * 
+	 *
 	 * @return 곱셈 연산을 수행한 결과를 반환합니다.
 	 */
 	Quat operator*(const Quat& q) const
@@ -221,6 +221,19 @@ struct Quat
 			+q.x * y - q.y * x + q.z * w + q.w * z,
 			-q.x * x - q.y * y - q.z * z + q.w * w
 		);
+	}
+
+
+	/**
+	 * @brief 쿼터니언과 벡터의 곱셈 연산을 수행합니다.
+	 *
+	 * @param vec 곱셈 연산을 수행할 벡터 값입니다.
+	 *
+	 * @return 곱셈 연산을 수행한 결과를 반환합니다.
+	 */
+	Vec3f operator*(const Vec3f& vec) const
+	{
+		return v * 2.0f * Vec3f::Dot(v, vec) + vec * (w * w - Vec3f::Dot(v, v)) + Vec3f::Cross(v, vec) * 2.0f * w;
 	}
 
 
