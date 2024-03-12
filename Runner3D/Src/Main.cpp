ee7bd32bacac97ae459cac9b2e8e6a2fcd00dff1
CHOIJIONE\choij <choijione02@gmail.com>
2024-03-12 14:20:09 +0900

    삼각형 렌더링을 위한 테스트 코드 추가
    - 현재 심각한 버그 발생

--- Runner3D/Src/Main.cpp
+++ Runner3D/Src/Main.cpp
@@ -12,11 +12,13 @@
 #include "Assertion.h"
 #include "GameTimer.h"
 #include "InputManager.h"
+#include "IndexBuffer.h"
 #include "RenderManager.h"
 #include "ResourceManager.h"
 #include "Shader.h"
 #include "SDLManager.h"
 #include "Texture2D.h"
+#include "VertexAttribute.h"
 
 bool bIsDone = false;
 GameTimer timer;
@@ -35,6 +37,31 @@ int32_t WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstan
 	RenderManager::Get().SetMultisampleMode(true);
 
 	InputManager::Get().AddWindowEventAction(EWindowEvent::CLOSE, [&]() {bIsDone = true; }, true);
+
+	std::vector<Vec3f> positions = 
+	{ 
+		Vec3f(-0.5f, -0.5f, 0.0f),
+		Vec3f(+0.5f, -0.5f, 0.0f), 
+		Vec3f(+0.0f, +0.5f, 0.0f) 
+	};
+	VertexAttribute<Vec3f> vertexPosition;
+	vertexPosition.Set(positions);
+
+	std::vector<Vec4f> colors = 
+	{ 
+		Vec4f(1.0f, 0.0f, 0.0f, 1.0f), 
+		Vec4f(0.0f, 1.0f, 0.0f, 1.0f), 
+		Vec4f(0.0f, 0.0f, 1.0f, 1.0f)
+	};
+	VertexAttribute<Vec4f> vertexColor;
+	vertexColor.Set(colors);
+
+	std::vector<uint32_t> indices = { 0, 1, 2 };
+	IndexBuffer indexBuffer;
+	indexBuffer.Set(indices);
+
+	RUID shaderID = ResourceManager::Get().Create<Shader>("Shader/Shader.vert", "Shader/Shader.frag");
+	Shader* shader = ResourceManager::Get().GetResource<Shader>(shaderID);
 	
 	timer.Reset();
 	while (!bIsDone)
@@ -43,6 +70,18 @@ int32_t WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstan
 		timer.Tick();
 
 		RenderManager::Get().BeginFrame(0.0f, 0.0f, 0.0f, 1.0f);
+
+		shader->Bind();
+		{
+			vertexPosition.Bind(shader->GetVertexAttribute("inPosition"));
+			vertexColor.Bind(shader->GetVertexAttribute("inColor"));
+
+			RenderManager::Get().Render(&indexBuffer, ERenderMode::Triangles);
+
+			vertexPosition.Unbind(shader->GetVertexAttribute("inPosition"));
+			vertexColor.Unbind(shader->GetVertexAttribute("inColor"));
+		}
+		shader->Unbind();
 		
 		RenderManager::Get().EndFrame();
 	}
