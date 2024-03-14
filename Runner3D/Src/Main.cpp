aa9ca7aef6f52578b1b4ce59bbb195977bc28c48
CHOIJIONE\choij <choijione02@gmail.com>
2024-03-14 19:19:51 +0900

    기능 구현 테스트 코드 추가

--- Runner3D/Src/Main.cpp
+++ Runner3D/Src/Main.cpp
@@ -12,6 +12,7 @@
 #include "Assertion.h"
 #include "GameTimer.h"
 #include "GeometryGenerator.h"
+#include "GeometryPass3D.h"
 #include "GLTFLoader.h"
 #include "InputManager.h"
 #include "RenderManager.h"
@@ -57,10 +58,15 @@ int32_t WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstan
 		vertices.push_back(VertexPositionNormalUv3D(position, normal, texCoords));
 	}
 
+	Pose resetPose = GLTFLoader::LoadRestPose(data);
+
 	GLTFLoader::Free(data);
 
 	RUID shaderID = ResourceManager::Get().Create<Shader>("Shader/Shader.vert", "Shader/Shader.frag");
 	Shader* shader = ResourceManager::Get().GetResource<Shader>(shaderID);
+
+	RUID geometryPass3D = ResourceManager::Get().Create<GeometryPass3D>();
+	GeometryPass3D* geometryPass = ResourceManager::Get().GetResource<GeometryPass3D>(geometryPass3D);
 
 	RUID textureID = ResourceManager::Get().Create<Texture2D>("Resource/Texture/Zombie.png");
 	Texture2D* texture = ResourceManager::Get().GetResource<Texture2D>(textureID);
@@ -74,21 +80,36 @@ int32_t WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstan
 		InputManager::Get().Tick();
 		timer.Tick();
 
+		Mat4x4 view = Mat4x4::LookAt(Vec3f(3.0f, 3.0f, 3.0f), Vec3f(0.0f, 0.0f, 0.0f), Vec3f(0.0f, 1.0f, 0.0f));
+		Mat4x4 projection = Mat4x4::Perspective(MathModule::ToRadian(45.0f), 1.25f, 0.1f, 100.0f);
+
 		RenderManager::Get().BeginFrame(0.0f, 0.0f, 0.0f, 1.0f);
 
-		shader->Bind();
+		//shader->Bind();
+		//{
+		//	texture->Active(0);
+
+		//	shader->SetUniform("world", Mat4x4::Scale(0.5f, 0.5f, 0.5f));
+		//	shader->SetUniform("view", view);
+		//	shader->SetUniform("projection", projection);
+
+		//	mesh->Bind();
+		//	glDrawElements(GL_TRIANGLES, mesh->GetIndexCount(), GL_UNSIGNED_INT, 0);
+		//	mesh->Unbind();
+		//}
+		//shader->Unbind();
+
+		geometryPass->Bind();
+		geometryPass->DrawGrid3D(view, projection, -4.0f, +4.0f, 1.0f, -4.0f, +4.0f, 1.0f, Vec4f(1.0f, 1.0f, 1.0f, 1.0f));
+
+		for (int32_t index = 0; index < resetPose.GetJointSize(); ++index)
 		{
-			texture->Active(0);
-
-			shader->SetUniform("world", Mat4x4::Scale(0.5f, 0.5f, 0.5f));
-			shader->SetUniform("view", Mat4x4::LookAt(Vec3f(3.0f, 3.0f, 3.0f), Vec3f(0.0f, 0.0f, 0.0f), Vec3f(0.0f, 1.0f, 0.0f)));
-			shader->SetUniform("projection", Mat4x4::Perspective(MathModule::ToRadian(45.0f), 1.25f, 0.1f, 100.0f));
-
-			mesh->Bind();
-			glDrawElements(GL_TRIANGLES, mesh->GetIndexCount(), GL_UNSIGNED_INT, 0);
-			mesh->Unbind();
+			Transform t = resetPose.GetLocalTransform(index);
+			Mat4x4 m = Transform::ToMat(t);
+			geometryPass->DrawSphere3D(m, view, projection, 0.05f, Vec4f(1.0f, 0.0f, 0.0f, 1.0f));
 		}
-		shader->Unbind();
+
+		geometryPass->Unbind();
 		
 		RenderManager::Get().EndFrame();
 	}
