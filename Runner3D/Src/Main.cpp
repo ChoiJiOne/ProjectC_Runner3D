a9da02ff08827f765394dd45c59aa1d858a323fc
DESKTOP-CIQEUKA\choij <choijione02@gmail.com>
2024-03-16 22:49:23 +0900

    캐릭터 애니메이션 테스트 코드 추가
    - 기존의 테스트 코드 삭제

--- Runner3D/Src/Main.cpp
+++ Runner3D/Src/Main.cpp
@@ -30,7 +30,7 @@
 bool bIsDone = false;
 GameTimer timer;
 
-Mat4x4 view = Mat4x4::LookAt(Vec3f(0.0f, 5.0f, 5.0f), Vec3f(0.0f, 0.0f, 0.0f), Vec3f(0.0f, 1.0f, 0.0f));
+Mat4x4 view = Mat4x4::LookAt(Vec3f(5.0f, 5.0f, 5.0f), Vec3f(0.0f, 0.0f, 0.0f), Vec3f(0.0f, 1.0f, 0.0f));
 Mat4x4 projection = Mat4x4::Perspective(MathModule::ToRadian(45.0f), 1.25f, 0.1f, 100.0f);
 
 int32_t WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR pCmdLine, _In_ int32_t nCmdShow)
@@ -51,26 +51,13 @@ int32_t WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstan
 	GeometryPass3D* geometryPass = ResourceManager::Get().GetResource<GeometryPass3D>(ResourceManager::Get().Create<GeometryPass3D>());
 	Shader* shader = ResourceManager::Get().GetResource<Shader>(ResourceManager::Get().Create<Shader>("Shader/Shader.vert", "Shader/Shader.frag"));
 
-	cgltf_data* data = GLTFLoader::LoadFromFile("Resource/Model/Zombie.gltf");
+	cgltf_data* data = GLTFLoader::LoadFromFile("Resource/Model/Running.gltf");
 	std::vector<GLTFLoader::SkinnedMeshData> skinnedMeshes = GLTFLoader::LoadSkinnedMeshData(data);
-	std::vector<VertexPositionNormalUvSkin3D> vertices;
-	std::vector<uint32_t> indices = skinnedMeshes[0].indices;
-
-	for (uint32_t index = 0; index < skinnedMeshes[0].positions.size(); ++index)
-	{
-		Vec3f position  = skinnedMeshes[0].positions[index];
-		Vec3f normal    = skinnedMeshes[0].normals[index];
-		Vec2f texcoords = skinnedMeshes[0].texCoords[index];
-		Vec4f weight    = skinnedMeshes[0].weights[index];
-		Vec4i joint     = skinnedMeshes[0].joints[index];
-
-		vertices.push_back(VertexPositionNormalUvSkin3D(position, normal, texcoords, weight, joint));
-	}
-
+	Skeleton skeleton = GLTFLoader::LoadSkeleton(data);
+	std::vector<Clip> clips = GLTFLoader::LoadAnimationClip(data);
 	GLTFLoader::Free(data);
 
-	Texture2D* texture = ResourceManager::Get().GetResource<Texture2D>(ResourceManager::Get().Create<Texture2D>("Resource/Texture/Zombie.png"));
-	SkinnedMesh* skinnedMesh = ResourceManager::Get().GetResource<SkinnedMesh>(ResourceManager::Get().Create<SkinnedMesh>(vertices, indices));
+	float playbackTime = 0.0f;
 
 	timer.Reset();
 	while (!bIsDone)
@@ -78,23 +65,25 @@ int32_t WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstan
 		InputManager::Get().Tick();
 		timer.Tick();
 
+		playbackTime = clips[0].Sample(skeleton.GetRestPose(), playbackTime + timer.GetDeltaSeconds() / 10.0f);
+
 		RenderManager::Get().BeginFrame(0.0f, 0.0f, 0.0f, 1.0f);
 
-		shader->Bind();
-		{
-			texture->Active(0);
-
-			shader->SetUniform("world", Mat4x4::Scale(0.5f, 0.5f, 0.5f));
-			shader->SetUniform("view", view);
-			shader->SetUniform("projection", projection);
-
-			skinnedMesh->Bind();
-			glDrawElements(GL_TRIANGLES, skinnedMesh->GetIndexCount(), GL_UNSIGNED_INT, 0);
-			skinnedMesh->Unbind();
-		}
-		shader->Unbind();
-
 		geometryPass->DrawGrid3D(view, projection, -3.0f, 3.0f, 1.0f, -3.0f, +3.0f, 1.0f, Vec4f(1.0f, 1.0f, 1.0f, 1.0f));
+
+		Pose& pose = skeleton.GetRestPose();
+		for (uint32_t index = 0; index < pose.GetJointSize(); ++index)
+		{
+			if (pose.GetParent(index) < 0)
+			{
+				continue;
+			}
+
+			Vec3f from = pose.GetGlobalTransform(index).position;
+			Vec3f to = pose.GetGlobalTransform(pose.GetParent(index)).position;
+
+			geometryPass->DrawLine3D(view, projection, from, to, Vec4f(1.0f, 0.0f, 0.0f, 1.0f));
+		}
 
 		RenderManager::Get().EndFrame();
 	}
