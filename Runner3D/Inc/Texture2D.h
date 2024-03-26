7c958034905b091e3f96079fd6432bd526be2d75
DESKTOP-CIQEUKA\choij <choijione02@gmail.com>
2024-03-27 07:08:22 +0900

    잘못 표시된 주석문 수정

--- Runner3D/Inc/Texture2D.h
+++ Runner3D/Inc/Texture2D.h
@@ -16,7 +16,7 @@ public:
 	 * @brief 이미지 파일을 읽고 텍스처 리소스를 생성합니다.
 	 *
 	 * @param path 이미지 파일의 경로입니다.
-	 * @param bIsVerticallyFlip 픽셀 버퍼를 수직으로 뒤집을지 확인합니다. 기본 값은 true입니다.
+	 * @param bIsVerticallyFlip 픽셀 버퍼를 수직으로 뒤집을지 확인합니다. 기본 값은 false입니다.
 	 */
 	explicit Texture2D(const std::string& path, bool bIsVerticallyFlip = false);
 
