56571e53bfd9f939c8c202d3d15ee133d768fde6
DESKTOP-CIQEUKA\choij <choijione02@gmail.com>
2024-03-28 08:50:38 +0900

    카메라 엔티티 추가 및 CPU 스키닝을 수행한 메시에 셰도우 효과를 적용하는 코드 추가

--- Runner3D/Src/Main.cpp
+++ Runner3D/Src/Main.cpp
@@ -2,8 +2,10 @@
 #include "Transform.h"
 
 #include "Assertion.h"
+#include "Camera.h"
 #include "Clip.h"
 #include "CrossFadeController.h"
+#include "EntityManager.h"
 #include "GameTimer.h"
 #include "GeometryGenerator.h"
 #include "GeometryPass3D.h"
@@ -23,13 +25,12 @@
 bool bIsDone = false;
 GameTimer timer;
 
-Mat4x4 view = Mat4x4::LookAt(Vec3f(10.0f, 10.0f, 10.0f), Vec3f(0.0f, 0.0f, 0.0f), Vec3f(0.0f, 1.0f, 0.0f));
-Mat4x4 projection = Mat4x4::Perspective(MathModule::ToRadian(45.0f), 1.25f, 0.1f, 100.0f);
+Camera* camera = nullptr;
 
 const uint32_t SHADOW_WIDTH = 1024;
 const uint32_t SHADOW_HEIGHT = 1024;
 
-Vec3f viewPosition = Vec3f(10.0f, 10.0f, 10.0f);
+Vec3f viewPosition = Vec3f(-10.0f, 10.0f, 0.0f);
 Vec3f lightPosition = Vec3f(0.0f, 5.0f, 0.0f);
 Vec3f lightDirection = Vec3f(0.0f, -1.0f, 0.0f);
 Vec4f lightColor = Vec4f(1.0f, 1.0f, 1.0f, 1.0f);
@@ -44,6 +45,7 @@ int32_t WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstan
 	InputManager::Get().Startup();
 	ResourceManager::Get().Startup();
 	RenderManager::Get().Startup();
+	EntityManager::Get().Startup();
 
 	RenderManager::Get().SetAlphaBlendMode(true);
 	RenderManager::Get().SetDepthMode(true);
@@ -51,12 +53,18 @@ int32_t WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstan
 
 	InputManager::Get().AddWindowEventAction(EWindowEvent::CLOSE, [&]() {bIsDone = true; }, true);
 
+	camera = EntityManager::Get().CreateEntity<Camera>(
+		Vec3f(-10.0f, +5.0f, 0.0f), Vec3f(+1.0f, -0.2f, 0.0f), Vec3f( +0.0f, +1.0f, 0.0f),
+		MathModule::ToRadian(45.0f), 1.25f, 0.1f, 100.0f
+	);
+
 	std::vector<VertexPositionNormalUv3D> vertices;
 	std::vector<uint32_t> indices;
 
 	Shader* shadowPass = ResourceManager::Get().CreateResource<Shader>("Shader/ShadowPass_NoSkinning.vert", "Shader/ShadowPass.frag");
 	Shader* lightPass = ResourceManager::Get().CreateResource<Shader>("Shader/LightPass_NoSkinning.vert", "Shader/LightPass.frag");
-	Texture2D* texture = ResourceManager::Get().CreateResource<Texture2D>("Resource/Texture/earth.png");
+	Texture2D* texture0 = ResourceManager::Get().CreateResource<Texture2D>("Resource/Texture/earth.png");
+	Texture2D* texture1 = ResourceManager::Get().CreateResource<Texture2D>("Resource/Texture/Female.png");
 	ShadowMap* shadowMap = ResourceManager::Get().CreateResource<ShadowMap>(SHADOW_WIDTH, SHADOW_HEIGHT);
 
 	GeometryGenerator::CreateSphere(1.0f, 40, vertices, indices);
@@ -65,64 +73,92 @@ int32_t WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstan
 	GeometryGenerator::CreateCube(Vec3f(10.0f, 1.0f, 10.0f), vertices, indices);
 	StaticMesh* cube = ResourceManager::Get().CreateResource<StaticMesh>(vertices, indices);
 
+	cgltf_data* data = GLTFLoader::LoadFromFile("Resource/Model/Character.gltf");
+	std::vector<GLTFLoader::SkinnedMeshData> skinnedMeshes = GLTFLoader::LoadSkinnedMeshData(data);
+	std::vector<VertexPositionNormalUvSkin3D> skinVertices;
+	std::vector<uint32_t> skinIndices = skinnedMeshes[0].indices;
+
+	for (uint32_t index = 0; index < skinnedMeshes[0].positions.size(); ++index)
+	{
+		Vec3f position = skinnedMeshes[0].positions[index];
+		Vec3f normal = skinnedMeshes[0].normals[index];
+		Vec2f texcoords = skinnedMeshes[0].texCoords[index];
+		Vec4f weight = skinnedMeshes[0].weights[index];
+		Vec4i joint = skinnedMeshes[0].joints[index];
+
+		skinVertices.push_back(VertexPositionNormalUvSkin3D(position, normal, texcoords, weight, joint));
+	}
+
+	SkinnedMesh* skinnedMesh = ResourceManager::Get().CreateResource<SkinnedMesh>(skinVertices, skinIndices, true);
+	Skeleton skeleton = GLTFLoader::LoadSkeleton(data);
+	std::vector<Clip> clips = GLTFLoader::LoadAnimationClip(data);
+
+	GLTFLoader::Free(data);
+
+	int32_t currentClip = 1;
+
+	CrossFadeContoller crossFadeContoller(skeleton);
+	crossFadeContoller.Play(&clips[currentClip]);
+	crossFadeContoller.Update(0.0f);
+
 	timer.Reset();
 	while (!bIsDone)
 	{
 		timer.Tick();
+
+		crossFadeContoller.Update(timer.GetDeltaSeconds() / 10.0f);
+		skinnedMesh->Skin(&crossFadeContoller.GetSkeleton(), &crossFadeContoller.GetCurrentPose());
+
 		InputManager::Get().Tick();
 
 		RenderManager::Get().BeginFrame(0.0f, 0.0f, 0.0f, 1.0f);
 		{
 			RenderManager::Get().SetViewport(0, 0, SHADOW_WIDTH, SHADOW_HEIGHT);
 			
-			shadowPass->Bind();
 			shadowMap->Bind();
 			shadowMap->Clear();
+			shadowPass->Bind();
 
 			shadowPass->SetUniform("view", lightView);
 			shadowPass->SetUniform("projection", lightProjection);
 
-			shadowPass->SetUniform("world", Mat4x4::Translation(-2.0f, +1.0f, +0.0f));
-			RenderManager::Get().RenderStaticMesh(sphere->GetID());
-
-			shadowPass->SetUniform("world", Mat4x4::Translation(+2.0f, +1.0f, +0.0f));
-			RenderManager::Get().RenderStaticMesh(sphere->GetID());
-
 			shadowPass->SetUniform("world", Mat4x4::Translation(+0.0f, -0.5f, 0.0f));
 			RenderManager::Get().RenderStaticMesh(cube->GetID());
 
-			shadowMap->Unbind();
+			shadowPass->SetUniform("world", Mat4x4::Identity());
+			RenderManager::Get().RenderSkinnedMesh(skinnedMesh->GetID());
+
 			shadowPass->Unbind();
+			shadowMap->Unbind();
 		}
 		{
 			RenderManager::Get().SetWindowViewport();
 
 			lightPass->Bind();
-			lightPass->SetUniform("view", view);
-			lightPass->SetUniform("projection", projection);
+			lightPass->SetUniform("view", camera->GetViewMatrix());
+			lightPass->SetUniform("projection", camera->GetProjectionMatrix());
 			lightPass->SetUniform("lightView", lightView);
 			lightPass->SetUniform("lightProjection", lightProjection);
 			lightPass->SetUniform("lightDirection", lightDirection);
 			lightPass->SetUniform("lightColor", lightColor.x, lightColor.y, lightColor.z);
-			lightPass->SetUniform("viewPosition", viewPosition);
+			lightPass->SetUniform("viewPosition", camera->GetEyePosition());
 
-			texture->Active(0);
+			texture0->Active(0);
 			shadowMap->Active(1);
-
-			lightPass->SetUniform("world", Mat4x4::Translation(-2.0f, +1.0f, +0.0f));
-			RenderManager::Get().RenderStaticMesh(sphere->GetID());
-
-			lightPass->SetUniform("world", Mat4x4::Translation(+2.0f, +1.0f, +0.0f));
-			RenderManager::Get().RenderStaticMesh(sphere->GetID());
 
 			lightPass->SetUniform("world", Mat4x4::Translation(+0.0f, -0.5f, 0.0f));
 			RenderManager::Get().RenderStaticMesh(cube->GetID());
+
+			texture1->Active(0);
+			lightPass->SetUniform("world", Mat4x4::Identity());
+			RenderManager::Get().RenderSkinnedMesh(skinnedMesh->GetID());
 
 			lightPass->Unbind();
 		}
 		RenderManager::Get().EndFrame();
 	}
 	
+	EntityManager::Get().Shutdown();
 	ResourceManager::Get().Shutdown();
 	RenderManager::Get().Shutdown();
 	InputManager::Get().Shutdown();
